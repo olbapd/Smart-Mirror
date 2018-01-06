294a67536cd258d728c3fb11ad41b7d2a1c5aceb
olbapd <#>
2018-01-05 22:21:12 -0600

    Made Get Function for all data.

--- weather.py
+++ weather.py
@@ -2,9 +2,9 @@ import requests
 import traceback
 import json
 
-
 lat=''
 lon=''
+
 def get_ip():
     try:
         ip_url = "http://jsonip.com/"
@@ -13,29 +13,32 @@ def get_ip():
         return ip_json['ip']
     except Exception as e:
         traceback.print_exc()
-        return "Error: %s. Cannot get ip." % e
+        print( "Error: %s. Cannot get ip." % e)
+        return 0
 
 def get_lat_long():
-    location_req_url = "http://freegeoip.net/json/%s" % get_ip()
-    r = requests.get(location_req_url)
-    location_obj = json.loads(r.text)
     global lat, lon
-    lat = str(location_obj['latitude'])
-    lon = str(location_obj['longitude'])
+    ip= get_ip()
+    if ip != 0:
+        location_req_url = "http://freegeoip.net/json/%s" % get_ip()
+        r = requests.get(location_req_url)
+        location_obj = json.loads(r.text)
+        lat = str(location_obj['latitude'])
+        lon = str(location_obj['longitude'])
+        return 1
+    else:
+        return 0
 
-get_lat_long()
-
-api='http://api.openweathermap.org/data/2.5/weather?appid=18e32a0b3135595acace9d680b9f3d29&lat='+lat+'&lon='+lon
-
-wData=requests.get(api).json()
-wInfo=wData['weather'][0]['description']
-wTemp=str(int(wData['main']['temp'])- 273.15).split('.')[0]
-wWindSpeed=str(wData['wind']['speed'])
-wCountry=wData['sys']['country']
-wCity=wData['name']
-print("General info: "+wInfo)
-print("Temperature: "+wTemp)
-print("Wind Speed: "+wWindSpeed)
-print("Country: "+wCountry)
-print("City: "+wCity)
+def get_Data():
+    if get_lat_long() == 1:
+        api='http://api.openweathermap.org/data/2.5/weather?appid=18e32a0b3135595acace9d680b9f3d29&lat='+lat+'&lon='+lon
+        wData=requests.get(api).json()
+        wInfo=wData['weather'][0]['description']
+        wTemp=str(int(wData['main']['temp'])- 273.15).split('.')[0]
+        wWindSpeed=str(wData['wind']['speed'])
+        wCountry=wData['sys']['country']
+        wCity=wData['name']
+        return [wInfo,wTemp,wWindSpeed,wCountry,wCity]
+    else:
+        return [None]
 
