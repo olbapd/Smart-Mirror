4da2c270c536acd58f2b5070dffa20d4dc3f1473
olbapd <#>
2018-01-05 18:32:23 -0600

    Obtains the Lat-Long.

--- GUI.py
+++ GUI.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from datetime import datetime
 import threading
+import calendar
 import time
 import requests
+import traceback
+import json
+
 
 updates=True
 currentDateInfo=datetime.now().strftime("%d-%m-%Y")
@@ -12,28 +16,42 @@ mainView=Tk()
 mainView.geometry('500x700')
 mainView.configure(background='black')
 
-myDate=Label(mainView,text=str(currentDateInfo),fg="gray",bg="black",font=("Comic Sans MS", 50))
+myDate=Label(mainView,text=str(currentDateInfo),fg="white",bg="black",font=("Comic Sans MS", 50))
 myDate.pack()
-myTime=Label(mainView,text=str(currentTimeInfo),fg="gray",bg="black",font=("Comic Sans MS", 50))
+myTime=Label(mainView,text=str(currentTimeInfo),fg="white",bg="black",font=("Comic Sans MS", 50))
 myTime.pack()
 
-api='http://api.openweathermap.org/data/2.5/weather?appid=18e32a0b3135595acace9d680b9f3d29&q=Mumbai'
-wData=requests.get(api).json()
-weather=wData['weather'][0]['main']
-print(weather)
-
-
+print(str(currentDateInfo)) 
+print(str(currentTimeInfo))
+def get_ip():
+    try:
+        ip_url = "http://jsonip.com/"
+        req = requests.get(ip_url)
+        ip_json = json.loads(req.text)
+        return ip_json['ip']
+    except Exception as e:
+        traceback.print_exc()
+        return "Error: %s. Cannot get ip." % e
 def updateDate():
     while (updates):
-        currentDateInfo=datetime.now().strftime("%d-%m-%Y")
-        currentTimeInfo=datetime.now().strftime("%H:%M:%S")
+        currentDateInfo=datetime.now().strftime("%a %d %b %Y")
+        currentTimeInfo=datetime.now().strftime("%H:%M %p")
         myDate.config(text=str(currentDateInfo))
         myTime.config(text=str(currentTimeInfo))
+        print("Running")
         time.sleep(0.5)
 t=threading.Thread(target=updateDate)
 t.start()
+        
+def get_lat_long():
+    location_req_url = "http://freegeoip.net/json/%s" % get_ip()
+    r = requests.get(location_req_url)
+    location_obj = json.loads(r.text)
+    lat = location_obj['latitude']
+    lon = location_obj['longitude']
+    print(lat)
+    print(lon)
 
-
-
+get_lat_long()
 
 mainView.mainloop()
