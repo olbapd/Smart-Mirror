6591a8c5fe59bb96d03eb9e41382c4e06c5ae60c
marcf98 <32026019+marcf98@users.noreply.github.com>
2018-01-05 22:09:53 -0600

    News implementation
    Automatic or by search

--- news.py
+++ news.py
@@ -1,24 +1,57 @@
 import requests
+import time
 
 
 #Info suministrated by Newsapi.org
-#ce579b2e7ae441d0830cce3bc04fd534
+
+#Automatic news True
+news=True
+
+
+#Gets the information form the source list 
 def getNews():
-    sources=["bbc-news","espn","cnn","fox-sports","marca","the-verge"]
-    
-    url = ('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ce579b2e7ae441d0830cce3bc04fd534')
-    cont=0
-    while (cont<3):
-        nData = requests.get(url).json()
-        nAuthor=nData['articles'][cont]['author']
-        nTitle=nData['articles'][cont]['title']
-        nDescription=nData['articles'][cont]['description']
-        cont+=1
-        print (nAuthor)
-        print (nTitle)
-        print (nDescription)
-    else:
-        print("There are no news left")
-    
+    sources=["bbc-news","espn","cnn","fox-sports","marca","the-verge","crypto-coins-news","engadget","ign","abc-news","business-insider",
+             "mtv-news","national-geographic","techradar"]
+    contSource=0
+    while(news):
+        if (contSource<len(sources)):
+            url = ('https://newsapi.org/v2/top-headlines?sources='+sources[contSource]+'&apiKey=ce579b2e7ae441d0830cce3bc04fd534')
+            contSource+=1
+            contArticle=0
+            while (contArticle<3):
+                nData = requests.get(url).json()
+                nAuthor=nData['articles'][contArticle]['source']['name']
+                nTitle=nData['articles'][contArticle]['title']
+                nDescription=nData['articles'][contArticle]['description']
+                if(contArticle==0):
+                    print ("News from: "+nAuthor)
+                print (nTitle)
+                print (nDescription)
+                print ("")
+                contArticle+=1
+            else:
+                print("-------------------------------------------------------")
+                time.sleep(5)
+        else:
+            contSource=0
+            time.sleep(10)
 
 getNews()
+
+#Makes a search for news depending on the keyword
+def searchNews():
+    keyword=input("Search for: ")
+    url='https://newsapi.org/v2/everything?q='+keyword+'&apiKey=ce579b2e7ae441d0830cce3bc04fd534'
+    contArticle=0
+    while (contArticle<3):
+        nData = requests.get(url).json()
+        nAuthor=nData['articles'][contArticle]['source']['name']
+        nTitle=nData['articles'][contArticle]['title']
+        nDescription=nData['articles'][contArticle]['description']
+        print ("News from: "+nAuthor)
+        print (nTitle)
+        print (nDescription)
+        contArticle+=1
+    else:
+        print("-------------------------------------------------------")
+        time.sleep(5)
