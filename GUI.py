d47ce43330d91fbcbd6db4c2039849504dd62eb5
olbapd <#>
2018-01-05 21:01:21 -0600

    Fixed position of weather.

--- GUI.py
+++ GUI.py
@@ -69,12 +69,26 @@ def start_Window():
             time_Label.config(text=str(currentTimeInfo)) #Updates Time
             time.sleep(10)
 	
-    def updateWheather():
-        a=1
+    def updateWeather():
+        currently_Label.config(text="Rainy")
+        forecast_Label.config(text="Not Rainy")
+        temperature_Label.config(text="20 C")
+        location_Label.config(text="Cannot Pinpoint Location")
+
+        image = Image.open(weather_icon['clear-day']) #Must change this
+        image = image.resize((100, 100), Image.ANTIALIAS)
+        image = image.convert('RGB')
+        photo = ImageTk.PhotoImage(image)
+        icon_Label.config(image=photo)
+
 	
     #Frames
     topFrame = Frame(mainView, background = 'black')
     topFrame.pack(side = TOP, fill=BOTH, expand = YES)
+    top_rigt_Frame=Frame(topFrame, background = 'black')
+    top_rigt_Frame.pack(side = RIGHT, fill=BOTH, expand = YES)
+    top_left_Frame=Frame(topFrame, background = 'black')
+    top_left_Frame.pack(side = LEFT, fill=BOTH, expand = YES)
     bottomFrame = Frame(mainView, background = 'black')
     bottomFrame.pack(side = BOTTOM, fill=BOTH, expand = YES)
 
@@ -83,41 +97,35 @@ def start_Window():
     mainView.bind("<Escape>", end_fullscreen)
 
     #Time
-    time_Label = Label(topFrame, font=(font_type, large_text_size), fg="white", bg="black")
+    time_Label = Label(top_rigt_Frame, font=(font_type, large_text_size), fg="white", bg="black")
     time_Label.pack(side=TOP, anchor=E)
-    print(time_Label)
     #Day of Week
-    dayOWLbl = Label(topFrame, text=currentDay, font=(font_type, small_text_size), fg="white", bg="black")
+    dayOWLbl = Label(top_rigt_Frame, text=currentDay, font=(font_type, small_text_size), fg="white", bg="black")
     dayOWLbl.pack(side=TOP, anchor=E)
 
     #Date
-    date_Label = Label(topFrame, text=currentDate, font=(font_type, small_text_size), fg="white", bg="black")
+    date_Label = Label(top_rigt_Frame, text=currentDate, font=(font_type, small_text_size), fg="white", bg="black")
     date_Label.pack(side=TOP, anchor=E)
     
     #Weather
-    degree_Frame = Frame(mainView, bg="black")
+    degree_Frame = Frame(top_left_Frame, bg="black")
     degree_Frame.pack(side=TOP, anchor=W)
     temperature_Label = Label(degree_Frame, font=(font_type, xlarge_text_size), fg="white", bg="black")
     temperature_Label.pack(side=LEFT, anchor=N)
     icon_Label = Label(degree_Frame, bg="black")
     icon_Label.pack(side=LEFT, anchor=N, padx=20)
-    currently_Label = Label(mainView, font=(font_type, medium_text_size), fg="white", bg="black")
+    currently_Label = Label(top_left_Frame, font=(font_type, medium_text_size), fg="white", bg="black")
     currently_Label.pack(side=TOP, anchor=W)
-    forecast_Label = Label(mainView, font=(font_type, small_text_size), fg="white", bg="black")
+    forecast_Label = Label(top_left_Frame, font=(font_type, small_text_size), fg="white", bg="black")
     forecast_Label.pack(side=TOP, anchor=W)
-    locationLabel = Label(mainView, font=(font_type, small_text_size), fg="white", bg="black")
-    locationLabel.pack(side=TOP, anchor=W)
-
-    #Insert weather imgae
-    image = Image.open(weather_icon['clear-day']) #Must change this
-    image = image.resize((100, 100), Image.ANTIALIAS)
-    image = image.convert('RGB')
-    photo = ImageTk.PhotoImage(image)
-    icon_Label = Label(degree_Frame, bg="black",image=photo)
-    icon_Label.pack(side=LEFT, anchor=N, padx=20)
-
-    t=threading.Thread(target=updateDate)
-    t.start()
+    location_Label = Label(top_left_Frame, font=(font_type, small_text_size), fg="white", bg="black")
+    location_Label.pack(side=TOP, anchor=W)
+    
+    #Runs threads
+    t_date=threading.Thread(target=updateDate)
+    t_weather=threading.Thread(target=updateWeather)
+    t_date.start()
+    t_weather.start()
 
     mainView.mainloop()
  
